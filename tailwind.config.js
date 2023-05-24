6afcfc46bf3d476c7d7175f857f778cb44bf47dc
GhabrielBender <ghabrielbmutti@gmail.com>
2023-05-24 14:55:56 -0300

    chore: Adding NativeWind library

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./components/home/nearby/Nearbyjobs.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
